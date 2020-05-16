9793ac5ccfba1f95880631948a4e9a0c1c2e8656
Yannis <admin@coding-hard.com>
2020-05-16 22:17:08 +0200

    add other html stuff

---
+++ uebung_php/auswertung.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="de">
+    <head>
+        <title>Auswertung</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+
+<p>
+
+<?php
+    echo "Hallo " . $_GET["name"];
+?>
+
+</p>
+
+
+    </body>
+</html>
